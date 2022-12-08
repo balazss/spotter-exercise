e4d0d8e88e2ee95a42d015b3a4534693e86e6f91
Balazs Sipocz <balazs.sipocz@me.com>
2022-12-08 11:22:20 -0800

    Added Target Groups and Security Groups

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,3 +35,82 @@ module "target_group_server_blue" {
   health_check_path   = "/health"
   health_check_port   = var.port_app_server
 }
+
+# ------- Creating Target Group for the server ALB green environment -------
+module "target_group_server_green" {
+  source              = "./modules/ALB"
+  create_target_group = true
+  name                = "tg-${var.environment_name}-s-g"
+  port                = 80
+  protocol            = "HTTP"
+  vpc                 = module.networking.aws_vpc
+  tg_type             = "ip"
+  health_check_path   = "/status"
+  health_check_port   = var.port_app_server
+}
+
+# ------- Creating Target Group for the client ALB blue environment -------
+module "target_group_client_blue" {
+  source              = "./modules/ALB"
+  create_target_group = true
+  name                = "tg-${var.environment_name}-c-b"
+  port                = 80
+  protocol            = "HTTP"
+  vpc                 = module.networking.aws_vpc
+  tg_type             = "ip"
+  health_check_path   = "/"
+  health_check_port   = var.port_app_client
+}
+
+# ------- Creating Target Group for the client ALB green environment -------
+module "target_group_client_green" {
+  source              = "./modules/ALB"
+  create_target_group = true
+  name                = "tg-${var.environment_name}-c-g"
+  port                = 80
+  protocol            = "HTTP"
+  vpc                 = module.networking.aws_vpc
+  tg_type             = "ip"
+  health_check_path   = "/"
+  health_check_port   = var.port_app_client
+}
+
+# ------- Creating Security Group for the server ALB -------
+module "security_group_alb_server" {
+  source              = "./modules/SecurityGroup"
+  name                = "alb-${var.environment_name}-server"
+  description         = "Controls access to the server ALB"
+  vpc_id              = module.networking.aws_vpc
+  cidr_blocks_ingress = ["0.0.0.0/0"]
+  ingress_port        = 80
+}
+
+# ------- Creating Security Group for the client ALB -------
+module "security_group_alb_client" {
+  source              = "./modules/SecurityGroup"
+  name                = "alb-${var.environment_name}-client"
+  description         = "Controls access to the client ALB"
+  vpc_id              = module.networking.aws_vpc
+  cidr_blocks_ingress = ["0.0.0.0/0"]
+  ingress_port        = 80
+}
+
+# ------- Creating Server Application ALB -------
+module "alb_server" {
+  source         = "./modules/ALB"
+  create_alb     = true
+  name           = "${var.environment_name}-ser"
+  subnets        = [module.networking.public_subnets[0], module.networking.public_subnets[1]]
+  security_group = module.security_group_alb_server.sg_id
+  target_group   = module.target_group_server_blue.arn_tg
+}
+
+# ------- Creating Client Application ALB -------
+module "alb_client" {
+  source         = "./modules/ALB"
+  create_alb     = true
+  name           = "${var.environment_name}-cli"
+  subnets        = [module.networking.public_subnets[0], module.networking.public_subnets[1]]
+  security_group = module.security_group_alb_client.sg_id
+  target_group   = module.target_group_client_blue.arn_tg
+}
