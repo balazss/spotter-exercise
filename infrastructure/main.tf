258d053eca4a4e7fc96aa9958169fe1c2a5af327
Balazs Sipocz <balazs.sipocz@me.com>
2022-12-08 11:15:59 -0800

    Networking module

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -7,3 +7,25 @@ provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
 }
+
+# ------- Random numbers intended to be used as unique identifiers for resources -------
+resource "random_id" "RANDOM_ID" {
+  byte_length = "2"
+}
+
+# ------- Account ID -------
+data "aws_caller_identity" "id_current_account" {}
+
+# ------- Networking -------
+module "networking" {
+  source = "./Modules/Networking"
+  cidr   = ["10.120.0.0/16"]
+  name   = var.environment_name
+}
+
+# ------- Networking -------
+module "networking" {
+  source = "./modules/Networking"
+  cidr   = ["10.120.0.0/16"]
+  name   = var.environment_name
+}
